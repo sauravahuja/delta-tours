d7986ae794be71361ae08589fe2abc9906b277d1
Saurav Ahuja <sauravahuja0412@gmail.com>
2020-05-12 22:23:35 +0530

    Final Commit For Mobile Responsice Navbar

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -6,7 +6,7 @@ if (module.hot) {
 //TESTING
 console.log("Hello world");
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('#hero-slider').owlCarousel({
         autoplay: 'true',
         items: 1,
@@ -16,10 +16,10 @@ $(document).ready(function () {
     })
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('#testimonial-wrapper').owlCarousel({
         autoplay: 'true',
-        items: 3,
+        items: 1,
         loop: 'true',
         smartSpeed: 500,
         dots: 'false',
@@ -27,7 +27,7 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
-            600: {
+            800: {
                 items: 2
             },
             1000: {
@@ -37,37 +37,51 @@ $(document).ready(function () {
     })
 });
 
-$(document.addEventListener("scroll",function(){
+$(document.addEventListener("scroll", function() {
     const navbar = document.getElementById('navigation-bar');
-    if(scrollY>'200'){
+    if (scrollY > '200') {
         navbar.classList.add('nav-on-scroll');
-    }
-    else{
+    } else {
         navbar.classList.remove('nav-on-scroll');
     }
-        
+
 }));
 
-$(document).ready(function(){
+$(document).ready(function() {
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
-  });
+});
+
+
+
+const ham = document.querySelector(".ham-burger");
+const nav = document.querySelector(".mobile-nav");
+ham.addEventListener('click', function() {
+    console.log(nav);
+    nav.classList.toggle("mobile-nav-list");
+})
+$(document).ready(function() {
+    const bar = document.querySelector('.bar');
+    bar.addEventListener('click', function() {
+        bar.classList.toggle('bar-close');
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
 const path = require('path');
 const webpack = require('webpack');
 const postCSSPlugins = [
-	require('postcss-import'),
-	require('postcss-mixins'),
-	require('postcss-simple-vars'),
-	require('postcss-nested'),
-	require('autoprefixer')
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
 ];
 
 
 module.exports = {
-	entry: './app/assets/scripts/app.js',
-	output: {
-		filename: "app.bundled.js",
-		path: path.resolve(__dirname, "app"),
-	},
-	devServer: {
-		before: function (app, server) {
-			server._watch('./app/**/*.html')
-		},
-		contentBase: path.join(__dirname, "app"),
-		hot: true,
-		port: 4000,
-		host: '0.0.0.0' //it is broadcast address it will allow my website to broadcast
-	},
-	mode: "development",
-	module: {
-		rules: [{
-			test: /\.css$/i,
-			use: [
-				'style-loader',
-				'css-loader?url=false',
-				{
-					loader: 'postcss-loader',
-					options: {
-						plugins: postCSSPlugins
-					}
-				}
-			],
-		}]
-	}
+    entry: './app/assets/scripts/app.js',
+    output: {
+        filename: "app.bundled.js",
+        path: path.resolve(__dirname, "app"),
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0' //it is broadcast address it will allow my website to broadcast
+    },
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader?url=false',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: postCSSPlugins
+                    }
+                }
+            ],
+        }]
+    }
 }
